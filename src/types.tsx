a9546d8913996b1fb1d9bf66cf367c0d6d38681c
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-10-21 17:20:16 +0200

    chore: change role and link to class

--- src/types.tsx
+++ src/types.tsx
@@ -1,19 +1,40 @@
-export type Role = {
+export abstract class Component {
+    type: string;
+
+    constructor(type: string) {
+        this.type = type;
+    }
+}
+
+export class Role extends Component {
     name: string;
     extends: Role | undefined;
+
+    constructor(name: string, extension: Role | undefined) {
+        super("role");
+        this.name = name;
+        this.extends = extension
+    }
 }
 
 export type LinkLabel = "communication" | "authority" | "acquaintance"
 
-export type Link = {
+export class Link extends Component {
     label: LinkLabel;
     from: Role;
     to: Role;
+
+    constructor(label: LinkLabel, from: Role, to: Role) {
+        super("link");
+        this.label = label;
+        this.from = from;
+        this.to = to;
+    }
 }
 
-export type Constraint = {}
+export type Constraint = Component & {}
 
-export type Group = {
+export type Group = Component & {
     name: string;
     roles: Array<Role>;
     links: Array<Link>;
