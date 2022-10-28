b3a548f41af35aa7c022f5190dc9905a780db5da
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-10-28 14:16:42 +0200

    chore: add structural diagram

--- src/components/structural/Diagram.tsx
+++ src/components/structural/Diagram.tsx
@@ -1,41 +1,103 @@
 import React from "react";
 import CytoscapeComponent from "react-cytoscapejs";
-import {Component, Link, Role} from "../../types";
+import {Component, Group, Link, Role} from "../../types";
 import {Core, ElementDefinition} from "cytoscape";
+import cytoscape from "cytoscape";
+import edgehandles from 'cytoscape-edgehandles';
+import compoundDragAndDrop from 'cytoscape-compound-drag-and-drop';
+import * as nodesStyle from "../../style/cytoscape/style.json";
 
 type DiagramProps = {
     elements: Array<Component>
+    link: {
+        from: string,
+        to: string,
+    }
+    onLinkCreation: (from: string, to: string) => void
+    onPropertyChange: (p: string, v: string) => void
+    onRoleToGroupAddition: (r: string, g: string) => void
+    onRoleFromGroupRemove: (r: string, g: string) => void
 }
 
 class Diagram extends React.Component<DiagramProps, {}> {
     private cy: Core
 
-    presentation(c: Component): ElementDefinition {
-        if (c instanceof Role) {
-            return { data: { id: c.name, label: c.name }}
+    constructor(props) {
+        super(props);
+        this.presentation = this.presentation.bind(this)
+    }
+
+    presentation(c: Component, group: string | undefined = undefined): ElementDefinition[] {
+        if (!c) return []
+        switch(c.type) {
+            case "role":
+                const r = c as Role
+                return [{ data: { id: r.name, label: r.name, role: true, parent: group } }]
+            case "group":
+                const g = c as Group
+                return [{ data: { id: g.name, label: g.name, group: true } }, ...Array.from(g.roles).flatMap(e => this.presentation(e, g.name))]
+            case "link":
+                const l = c as Link
+                return [{ data: { id: l.from.name + l.to.name + l.label, source: l.from.name, target: l.to.name, label: l.label, link: true, arrow: "triangle" } }]
+            default:
+                return []
         }
-        if (c instanceof Link) {
-            return { data: { source: c.from.name, target: c.to.name, label: c.label} }
-        }
-        return { data: { label: "" }}
     }
 
     componentDidMount() {
+        cytoscape.use(edgehandles)
+        cytoscape.use(compoundDragAndDrop)
+        const defaults = {
+            canConnect: () => true,
+        };
+        const options = {
+            grabbedNode: node => node._private.data.role,
+            dropTarget: node => node._private.data.group,
+            dropSibling: node => node._private.data.role,
+            newParentNode: (grabbedNode, dropSibling) => dropSibling
+        }
+        //@ts-ignore
+        const eh = this.cy.edgehandles(defaults)
+        //@ts-ignore
+        this.cy.compoundDragAndDrop(options);
+        this.cy.center()
         this.cy.on("free", (e) => {
             console.log(e.target._private.position)
+        })
+        this.cy.on("add", () => {
+            this.cy.center()
+        })
+        this.cy.on("cxttap", (e) => {
+            eh.start(e.target)
+        })
+        // @ts-ignore
+        this.cy.on("ehcomplete", (ev, s, t, e) => {
+            this.props.onLinkCreation(s._private.data.label, t._private.data.label)
+            this.cy.remove(e)
+        })
+        // @ts-ignore
+        this.cy.on("cdnddrop", (event, dropTarget, _) => {
+            if (dropTarget._private.data) {
+                this.props.onRoleToGroupAddition(event.target._private.data.label, dropTarget._private.data.label)
+            }
+        })
+        // @ts-ignore
+        this.cy.on("cdndout", (event, dropTarget, _) => {
+            this.props.onRoleFromGroupRemove(event.target._private.data.label, dropTarget._private.data.label)
         })
     }
 
     render() {
-        const elements = [
-            { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-            { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-        ];
-
         return (
             <div id="cy" className="diagram">
-                <CytoscapeComponent layout={ { name: "random" } } cy={(cy) => { this.cy = cy }} elements={this.props.elements.map(this.presentation)} style={ { width: "100%", height: "100%" } }/>
+                <CytoscapeComponent
+                    className="diagram-component"
+                    layout={ { name: "random", zoom: 1 } }
+                    cy={(cy) => { this.cy = cy }}
+                    elements={this.props.elements.flatMap(e => this.presentation(e))}
+                    // @ts-ignore
+                    stylesheet={nodesStyle}
+                />
             </div>
         );
     }
