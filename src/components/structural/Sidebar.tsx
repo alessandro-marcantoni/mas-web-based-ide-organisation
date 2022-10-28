587b17212e1fe2efde438e9dd2166da2b53a9750
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-10-28 14:16:10 +0200

    chore: add sidebar with role and group creation

--- src/components/structural/Sidebar.tsx
+++ src/components/structural/Sidebar.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import {Role} from "../../types";
 
-class Sidebar extends React.Component<{}, {}> {
-    render() {
-        return (
-            <div className="sidebar">
-                Hello
-            </div>
-        )
-    }
+type SidebarProps = {
+    roles: Array<Role>
+    role: string
+    group: string
+    addComponent: (c: string) => void
+    propertyChanged: (p: string, v: string) => void
+}
+
+const Sidebar = (p: SidebarProps) => {
+    return (<div className="sidebar">
+        { p.roles.map(r => <div key={r.name}>{r.name}</div>) }
+        <input type="text" value={p.role} onChange={(e) => p.propertyChanged("role", e.target.value)}/>
+        <button onClick={() => p.addComponent("role")}>Create role</button>
+        <input type="text" value={p.group} onChange={(e) => p.propertyChanged("group", e.target.value)}/>
+        <button onClick={() => p.addComponent("group")}>Create group</button>
+    </div>)
 }
 
 export default Sidebar;
