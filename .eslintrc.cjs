475a2f5db1440e55ab2b0691976b321dcde0ee93
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-11-17 11:29:23 +0100

    chore(deps): add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "no-case-declarations": "off",
+  },
+  ignorePatterns: ["*.config.js"]
+};
