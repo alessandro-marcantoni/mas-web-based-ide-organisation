75cfb8523cc6b95feeb5d11b9e487055a6bf1847
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-11-25 15:32:47 +0100

    chore: serialize roles and their extensions

--- src/main/utils/structural/serializer.ts
+++ src/main/utils/structural/serializer.ts
@@ -2,16 +2,16 @@ import {Cardinality, Compatibility, Component, Constraint, Group, Role} from "./
 import {getAllRoles, getGlobalGroups, option, shortName} from "./utils";
 import {List, toArray} from "scala-types/dist/list/list";
 
-export const serialize: (components: List<Component>, diagram: List<Component>) => string = (components, diagram) =>
+export const serialize: (diagram: List<Component>) => string = (diagram) =>
     formatXml("<structural-specification>" +
         "<role-definitions>" +
-            toArray(getAllRoles(components).map(role)).join("\n") +
+            toArray(getAllRoles(diagram).distinctBy(r => shortName(r.name)).map(role)).join("\n") +
         "</role-definitions>" + "\n" +
         toArray(getGlobalGroups(diagram).map(group)).join("\n") +
     "</structural-specification>")
 
 export const role: (role: Role) => string = (role) =>
-    `<role id="${role.name}">${option(role.extends).map(e => ` <extends role="${e.name}"/> `).getOrElse("")}</role>`
+    `<role id="${shortName(role.name)}">${option(role.extends).map(e => ` <extends role="${e.name}"/> `).getOrElse("")}</role>`
 
 export const group: (group: Group) => string = (group) =>
     `<group-specification id="${shortName(group.name)}" ${group.min === 0 ? "" : `min="${group.min}"`} ${group.max === Number.MAX_VALUE ? "" : `max="${group.max}"`}>${Object.entries(groupElements).map(e => group[e[0]].size > 0 ? e[1](group[e[0]]) : "").join("\n")}</group-specification>`
@@ -38,7 +38,7 @@ export const constraint: (constraint: Constraint) => string = (constraint) => {
             return `<cardinality ${ca.min === 0 ? "" : `min="${ca.min}"`} ${ca.max === Number.MAX_VALUE ? "" : `max="${ca.max}"`} object="${ca.object}" id="${shortName(ca.id)}"/>`
         case "compatibility":
             const co = constraint as Compatibility
-            return `<compatibility from="${shortName(co.from)}" to="${shortName(co.to)}" type="${co.type}" scope="${co.scope}" extends-subgroups="${co.extendsSubgroups}" bi-dir="${co.biDir}"/>`
+            return `<compatibility from="${shortName(co.from)}" to="${shortName(co.to)}" type="${co.constraint}" scope="${co.scope}" extends-subgroups="${co.extendsSubgroups}" bi-dir="${co.biDir}"/>`
         default:
             return ""
     }
