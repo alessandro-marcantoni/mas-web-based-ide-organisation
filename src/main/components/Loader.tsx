3634ee6e32213e31ec84a06bf6236f8f60141464
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-11-29 14:46:13 +0100

    chore: add initial page

--- src/main/components/Loader.tsx
+++ src/main/components/Loader.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+import {Box, Button, Toolbar} from "@mui/material"
+import React from "react"
+import {useNavigate} from "react-router-dom"
 
 function Loader() {
+    const navigate = useNavigate()
     return (
-        <div id="loading">
-            <img id="loading-logo"
-                src={ process.env.PUBLIC_URL + "/img/interactions-logo-reduced.png" } alt="Interactions Research Group, University of St.Gallen" />
-            <br />
-            <p>Loading...</p>
-        </div>
-    );
+        <>
+            <Toolbar />
+            <Box
+                sx={{
+                    p: 5,
+                    mt: 20,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <img
+                    id="loading-logo"
+                    width={300}
+                    src="/img/interactions-logo-reduced.png"
+                    alt="Interactions Research Group, University of St.Gallen"
+                />
+                <Button
+                    variant="contained"
+                    onClick={() => navigate("/structural")}
+                >
+                    Create organization
+                </Button>
+            </Box>
+        </>
+    )
 }
 
-export default Loader;
+export default Loader
