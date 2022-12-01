0a20d09bba37fd9a694cce7fb5b6359fb467149f
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-12-01 12:04:41 +0100

    chore: add sidebar and functional components

--- src/main/components/functional/Functional.tsx
+++ src/main/components/functional/Functional.tsx
@@ -1,33 +1,56 @@
 import React from "react"
 import { list } from "scala-types"
-import { DiagramEvent } from "../../utils/commons"
-import { presentation } from "../../utils/structural/cytoscape"
+import { List } from "scala-types/dist/list/list"
+import { Component, DiagramEvent, DiagramEventType } from "../../utils/commons"
+import { GoalCreationEvent } from "../../utils/functional/events"
+import { presentation } from "../../utils/functional/cytoscape"
 import Diagram from "../Diagram"
+import Sidebar from "./Sidebar"
+import { cddOptions, config, ehOptions } from "../../utils/structural/cytoscape"
 
 type FunctionalState = {
-    hi: string
+    components: List<Component>
 }
 
 class Functional extends React.Component<unknown, FunctionalState> {
     constructor(props) {
         super(props)
         this.state = {
-            hi: "hi",
+            components: list<Component>(),
         }
         this.onDiagramEvent = this.onDiagramEvent.bind(this)
+        this.onPropertyChange = this.onPropertyChange.bind(this)
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     onDiagramEvent(event: DiagramEvent): void {
-        // event handlers  here
+        switch (event.type) {
+            case DiagramEventType.GoalCreation:
+                const gce = event as GoalCreationEvent
+                console.log(gce)
+                break
+        }
+    }
+
+    onPropertyChange(property: string, value: unknown): void {
+        this.setState(state => {
+            return {
+                components: state.components,
+                [property]: value,
+            }
+        })
     }
 
     render() {
         return (
             <>
+                <Sidebar
+                    components={this.state.components}
+                    onEvent={this.onDiagramEvent}
+                    onPropertyChange={this.onPropertyChange}
+                />
                 <Diagram
-                    configuration={(cy, props) => console.log(cy, props)}
-                    elements={list()}
+                    configuration={(cy, props) => config(cy, ehOptions(), cddOptions(props), props)}
+                    elements={this.state.components}
                     onDiagramEvent={this.onDiagramEvent}
                     presentation={presentation}
                 />
