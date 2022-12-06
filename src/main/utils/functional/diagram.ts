5cfab01c51ba7c3182a21aa157d3510d249ed389
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-12-06 09:13:49 +0100

    chore: add handlers for goal creation and relations deletion

---
+++ src/main/utils/functional/diagram.ts
@@ -0,0 +1,28 @@
+import { List } from "scala-types/dist/list/list"
+import { Component } from "../commons"
+import { Goal } from "./entities"
+import { getAllGoals } from "./utils"
+
+export const addGoal: (components: List<Component>, goalName: string) => List<Component> = (components, goalName) =>
+    components.appended(new Goal(goalName))
+
+export const removeGoalRelation: (
+    components: List<Component>,
+    goalName: string,
+    other: string,
+    f: (other: string) => (g: Goal) => void
+) => List<Component> = (components, goalName, other, f) => {
+    const toRemove = getAllGoals(components).find(g => g.name === other)
+    getAllGoals(components)
+        .find(g => g.name === goalName)
+        .apply(f(other))
+    return toRemove.isDefined() ? components.appended(toRemove.get() as Goal) : components
+}
+
+export const subgoalRemover: (other: string) => (g: Goal) => void = other => g => {
+    g.subGoals = new Set(Array.from(g.subGoals).filter(s => s.name !== other))
+}
+
+export const dependencyRemover: (other: string) => (g: Goal) => void = other => g => {
+    g.dependsOn = new Set(Array.from(g.dependsOn).filter(s => s.name !== other))
+}
