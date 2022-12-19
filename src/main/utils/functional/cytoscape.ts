01f6815fbdf4547ff2cea1b187b17ff3f7719893
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2022-12-19 14:55:43 +0100

    chore: display goal operator for subgoals in diagram

--- src/main/utils/functional/cytoscape.ts
+++ src/main/utils/functional/cytoscape.ts
@@ -2,7 +2,7 @@ import { ElementDefinition } from "cytoscape"
 import { list, List } from "scala-types/dist/list/list"
 import { Component } from "../commons"
 import { fromSet } from "../structural/utils"
-import { Goal, PlanOperator } from "./entities"
+import { Goal, PlanOperator, stringOperator } from "./entities"
 
 export const presentation: (c: Component, cs: List<Component>) => List<ElementDefinition> = (c, cs) => {
     switch (c.type) {
@@ -24,7 +24,7 @@ const goal = (g: Goal) => {
     return {
         data: {
             id: g.name,
-            label: g.name,
+            label: g.name + (g.subGoals.size > 1 ? "\n\n" + "<" + stringOperator(g.operator) + ">" : ""),
             componentType: "goal",
         },
     }
