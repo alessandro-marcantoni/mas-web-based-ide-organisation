7180e2acd788c318a1e3361f289442aab6ea16ff
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2023-01-03 11:14:51 +0100

    chore: change navbar color

--- src/main/react/components/Header.tsx
+++ src/main/react/components/Header.tsx
@@ -4,8 +4,8 @@ import { AppBar, Container, Toolbar } from "@mui/material"
 function Header() {
     return (
         <>
-            <AppBar position="fixed" className="shadow" sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}>
-                <Container maxWidth="xl" sx={{ px: 3, m: 0 }}>
+            <AppBar position="fixed" className="shadow" sx={{ zIndex: theme => theme.zIndex.drawer + 1, backgroundColor: "#2e56a3" }}>
+                <Container maxWidth="xl" sx={{ px: 3, m: 0, width: "100%" }}>
                     <Toolbar disableGutters>
                         <img alt="" src="/img/interactions-logo-pure-white.png" height="50" />
                     </Toolbar>
