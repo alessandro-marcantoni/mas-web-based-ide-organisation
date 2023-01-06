1fb37dbc2c465e0413633aa1b7651deeea81712a
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2023-01-06 13:37:11 +0100

    chore: add support for or serialization and deserialization

--- src/main/typescript/io/serialization/functional.ts
+++ src/main/typescript/io/serialization/functional.ts
@@ -2,7 +2,7 @@ import { List, toArray } from 'scala-types/dist/list/list';
 import { formatXml } from "./common"
 import { Component } from "../../commons"
 import { getAllGoals } from '../../functional/utils';
-import { Goal } from '../../domain/functional';
+import { Goal, PlanOperator } from '../../domain/functional';
 
 export const serialize: (diagram: List<Component>) => string = diagram =>
     formatXml(
@@ -18,6 +18,21 @@ export const serialize: (diagram: List<Component>) => string = diagram =>
     )
 
 const goal: (goal: Goal) => string = goal =>
+    (goal.operator === PlanOperator.OR && goal.dependencies.size >= 2) ? orGoal(goal) : andGoal(goal)
+
+const orGoal: (goal: Goal) => string = goal =>
     "<goal id=\"" + goal.name + "\">" +
-        Array.from(goal.dependencies).map(dep => "<depends-on goal=\"" + dep + "\"/>").join("\n") +
+        "<depends-on goal=\"OR" + goal.name + "\"/>" +
+    "</goal>" +
+    "<goal id=\"" + OR_TOKEN + goal.name + "\">" +
+        "<plan operator=\"choice\">" +
+            Array.from(goal.dependencies).map(dep => "<goal id=\"" + dep + "\"/>").join("\n") +
+        "</plan>" +
     "</goal>"
+
+const andGoal: (goal: Goal) => string = goal =>
+    "<goal id=\"" + goal.name + "\">" +
+    Array.from(goal.dependencies).map(dep => "<depends-on goal=\"" + dep + "\"/>").join("\n") +
+    "</goal>"
+
+export const OR_TOKEN = "OR__"
