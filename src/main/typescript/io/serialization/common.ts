f0f900751ee4426ce8bd3d83c872001b34f8ef96
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2023-01-06 16:23:20 +0100

    chore: sending changes to backend when saving organizations

--- src/main/typescript/io/serialization/common.ts
+++ src/main/typescript/io/serialization/common.ts
@@ -12,3 +12,16 @@ export function formatXml(xml: string, tab: string = "\t") {
     })
     return formatted.substring(1, formatted.length - 3)
 }
+
+export const addHeader: (spec: string) => string = (spec) =>
+    formatXml(
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+        "<?xml-stylesheet href=\"os.xsl\" type=\"text/xsl\" ?>" + 
+        "<organisational-specification id=\"joj\" os-version=\"0.7\"" + "\n" +
+                "xmlns='http://moise.sourceforge.net/os'" + "\n" +
+                "xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'" + "\n" +
+                "xsi:schemaLocation='http://moise.sourceforge.net/os" + "\n" +
+                            "http://moise.sourceforge.net/xml/os.xsd'>" + "\n" +
+        spec + "\n" +
+        "</organisational-specification>"
+    )
