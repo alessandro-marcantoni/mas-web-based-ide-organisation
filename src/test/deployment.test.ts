a061d81bdc772f53a14317101bf96fb28a09643f
Alessandro Marcantoni <alessandr.marcanton2@studio.unibo.it>
2023-01-27 09:42:50 +0100

    chore: remove unused test

--- src/test/deployment.test.ts
+++
@@ -1,8 +0,0 @@
-import { list } from 'scala-types/dist/list/list';
-import { deployOrganization } from '../main/typescript/io/artifacts';
-
-describe("deploy an organization", () => {
-    test("deploy on Yggdrasil", async () => {
-        await deployOrganization("test", list())
-    })
-})
